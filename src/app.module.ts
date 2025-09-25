6109dbc7f45e73b617fa3733b26cde2a3094b6fa
Tristan Chin <tristan.chin@chintristan.io>
2025-09-24 23:10:38 -0400

    Setup Necord

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppService } from "./app.service";
 import { EnvModule } from "./env/env.module";
 import { ConfigModule } from "@nestjs/config";
 import { Env } from "./env/env";
+import { NecordModule } from "necord";
+import { IntentsBitField } from "discord.js";
 
 @Module({
   imports: [
@@ -11,6 +13,10 @@ import { Env } from "./env/env";
       isGlobal: true,
     }),
     EnvModule,
+    NecordModule.forRoot({
+      token: process.env.DISCORD_BOT_TOKEN!,
+      intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers],
+    }),
   ],
   providers: [AppService],
 })
