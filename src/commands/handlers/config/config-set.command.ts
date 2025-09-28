543a794b8ad7503fcf2e14e9e763d96d839ae1a0
Tristan Chin <tristan.chin@chintristan.io>
2025-09-28 14:11:23 -0400

    Limit config size

--- src/commands/handlers/config/config-set.command.ts
+++ src/commands/handlers/config/config-set.command.ts
@@ -6,12 +6,15 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigEditCommandGroup, ConfigCommand } from "./config.command";
 import { Context, Options, StringOption, Subcommand, type SlashCommandContext } from "necord";
+import { InteractionError } from "src/common/errors/interaction-error";
 
 class ConfigSetOptions {
   @StringOption({
     name: "key",
     description: "The configuration key to set",
     required: true,
+    min_length: 1,
+    max_length: 32,
   })
   key: string;
 
@@ -19,6 +22,8 @@ class ConfigSetOptions {
     name: "value",
     description: "The value to set for the configuration key",
     required: true,
+    min_length: 1,
+    max_length: 100,
   })
   value: string;
 }
@@ -33,6 +38,11 @@ export class ConfigSetCommand {
     description: "Set a configuration value",
   })
   async handleConfigSet(@Context() [interaction]: SlashCommandContext, @Options() { key, value }: ConfigSetOptions) {
+    if (this.configCommand.store.size >= 100 && !this.configCommand.store.has(key)) {
+      throw new InteractionError(
+        "You have reached the maximum of 100 configuration entries. Please delete an entry before adding a new one.",
+      );
+    }
     this.configCommand.store.set(key, value);
     return interaction.reply({ content: `Configuration updated: \`${key}\` set to \`${value}\`.` });
   }
